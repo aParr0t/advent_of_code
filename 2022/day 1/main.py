0901b75cf9d3e2d13577ba876433358f42712b70
aParrot <a.lapenas04@gmail.com>
2022-12-04 17:04:52 +0100

    uncommented code

--- 2022/day 1/main.py
+++ 2022/day 1/main.py
@@ -3,16 +3,16 @@ inp = [s.rstrip() for s in open("input.txt")]
 inp = [int(x) if x else "" for x in inp]
 
 # ----------part 1
-# max_num = 0
-# num = 0
-# for i in range(len(inp) + 1):
-#     if i == len(inp) or inp[i] == "":
-#         max_num = max(num, max_num)
-#         num = 0
-#     else:
-#         num += inp[i]
+max_num = 0
+num = 0
+for i in range(len(inp) + 1):
+    if i == len(inp) or inp[i] == "":
+        max_num = max(num, max_num)
+        num = 0
+    else:
+        num += inp[i]
 
-# print(f"Part 1 answer: {max_num}")
+print(f"Part 1 answer: {max_num}")
 # ----------part 1
 
 # ----------part 2
