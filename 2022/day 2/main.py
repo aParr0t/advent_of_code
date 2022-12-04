5d12c6359ed01c4f87f9d95e727c0c97a257294f
aParrot <a.lapenas04@gmail.com>
2022-12-04 17:04:22 +0100

    uncommented code

--- 2022/day 2/main.py
+++ 2022/day 2/main.py
@@ -14,19 +14,19 @@ choice_points = {
 # A, X = rock
 # B, Y = paper
 # C, Z = scissors
-# states = {
-#     "A": {"X": 1, "Y": 2, "Z": 0},
-#     "B": {"X": 0, "Y": 1, "Z": 2},
-#     "C": {"X": 2, "Y": 0, "Z": 1},
-# }
+states = {
+    "A": {"X": 1, "Y": 2, "Z": 0},
+    "B": {"X": 0, "Y": 1, "Z": 2},
+    "C": {"X": 2, "Y": 0, "Z": 1},
+}
 
-# total_score = 0
-# outcome_step = 3
-# for [opponent, player] in rounds:
-#     total_score += states[opponent][player] * outcome_step
-#     total_score += choice_points[player]
+total_score = 0
+outcome_step = 3
+for [opponent, player] in rounds:
+    total_score += states[opponent][player] * outcome_step
+    total_score += choice_points[player]
 
-# print(f"Part 1 answer: {total_score}")
+print(f"Part 1 answer: {total_score}")
 # ----------part 1
 
 # ----------part 2
