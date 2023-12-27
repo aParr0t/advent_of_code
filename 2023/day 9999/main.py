74f97d197e80a704c77e08e65e9dd94fb1e4a531
aParrot <a.lapenas04@gmail.com>
2023-12-27 01:08:27 +0100

    added useful libraries

--- 2023/day 9999/main.py
+++ 2023/day 9999/main.py
@@ -1,14 +1,23 @@
-# read input
-inp = [s.rstrip() for s in open("input.txt")]
+import functools
+import heapq
+import itertools
+import math
+import random
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
 
+# read input
+L = [s.rstrip() for s in open("input.txt")]
+G = [[c for c in s] for s in L]
+w, h = len(L[0]), len(L)
 
 # ----------part 1
 
-print(f"Part 1 answer: {res}")
+# print(f"Part 1 answer: {res}")
 # ----------part 1
 
 
 # ----------part 2
 
-print(f"Part 2 answer: {res}")
+# print(f"Part 2 answer: {res}")
 # ----------part 2
